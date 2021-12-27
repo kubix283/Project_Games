52294774815da62edd7e635bf6acbd0d8893fbec
kubix <maks05102000@gmail.com>
2021-12-27 19:01:27 +0100

    ..

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GameListView, GameDetailView
+from .views import GameListView, GameDetailView, SearchResultListView
 
 urlpatterns = [
     path('', GameListView.as_view(), name='game_list'),
-    path('<uuid:pk>', GameDetailView.as_view(), name='game_detail')
+    path('<uuid:pk>', GameDetailView.as_view(), name='game_detail'),
+    path('search/', SearchResultListView.as_view(), name='search')
 ]
--- games/views.py
+++ games/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.views.generic import ListView, DetailView  # new
 from .models import Game
 
@@ -16,3 +17,12 @@ class GameDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'games/game_detail.html'
     login_url = 'account_login'
     permission_required = 'games.special_status'
+
+class SearchResultListView(ListView):
+    model = Game
+    context_object_name = 'game_list'
+    template_name = 'games/search_result_list.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Game.objects.filter(Q(name__icontains=query) | Q(author__icontains=query))
